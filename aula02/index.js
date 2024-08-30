b10b7346ab39c45519123a4cf45473e1be80482f
Pedro Wrublak <pedrovili.w@gmail.com>
2024-08-30 13:05:35 -0300

    comentários

--- aula02/index.js
+++ aula02/index.js
@@ -1,5 +1,1 @@
-console.log("Pedro 'Wrublak'"); // String = Texto
-console.log('Pedro "Wrublak"');
-console.log(`'Pedro' "Wrublak"`);
-
-console.log(35, 15.85, 'Pedro Wrublak De Novo'); // Number = Número
+// Instalação dos softwares para Linux
