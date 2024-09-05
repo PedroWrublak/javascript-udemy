0486cb85db8d60727527349a9603265e9e8717f8
Pedro Wrublak <pedrovili.w@gmail.com>
2024-09-05 13:34:35 -0300

    var vs let

---
+++ aula09/index.js
@@ -0,0 +1,11 @@
+//Let vs Var - Primeira diferença
+
+// ECMAScript 2015 (ES6)
+var nome = 'Pedro';
+var nome = 'Wrublak';
+
+// var pode ser redeclarada, um dos motivos para não ser utilizada
+
+console.log(nome);
+
+nome = 'Pedro'; // NÃO FAÇA ISSO
