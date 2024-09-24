1831fe335c8037357f1fa5f80c98f981ce6c3c9f
Pedro Wrublak <pedrovili.w@gmail.com>
2024-09-24 17:13:23 -0300

    alert, confirm e prompt

---
+++ aula012/js/script.js
@@ -0,0 +1,16 @@
+// Alert, confirm e prompt (navegador)
+
+alert('Com a nossa mensagem.'); // ou window.alert('Mensagem');
+window.confirm('Confirma?');
+window.prompt('Digite seu nome:');
+
+const confirma = confirm('Realmente deseja apagar?');
+
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+const resultado = num1 + num2;
+alert(`O resultado foi: ${resultado}`);
+
