fb6f666b88b5ce5fec3d4aa115e86e4760c9c76f
Pedro Wrublak <pedrovili.w@gmail.com>
2024-09-27 09:28:40 -0300

    objeto math

---
+++ aula016-objeto_math/index.js
@@ -0,0 +1,17 @@
+// Objeto math
+
+let num1 = 9.54587;
+
+// let num2 = 9;
+// console.log(num2 ** (0.5));
+
+// let num2 = Math.floor(num1);
+// let num2 = Math.ceil(num1);
+// let num2 = Math.round(num1);
+
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// console.log(Math.PI);
+
+console.log(100 / 0);
