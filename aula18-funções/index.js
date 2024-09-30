0351713015b8ece5cecbe2e0927dcfd05f27b80a
Pedro Wrublak <pedrovili.w@gmail.com>
2024-09-30 15:47:36 -0300

    functions (básico)

---
+++ aula18-funções/index.js
@@ -0,0 +1,48 @@
+// Funções (básico)
+
+// Arrow function
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(81));
+
+/*
+const raiz = function(n) {
+    return  n ** 0.5;
+};
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(81));
+*/
+
+/*
+function soma(x = 1, y = 1) {
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma();
+console.log(resultado);
+*/
+
+/*
+console.log(soma(2, 2));
+console.log(soma(5, 2));
+console.log(soma(2, 6));
+*/
+
+/* 
+function saudacao(nome) {
+    console.log();
+    return `Bom dia ${nome}!`;
+}
+*/
+
+// const variavel = saudacao('Pedro');
+// console.log(variavel);
+
+// saudacao('Pedro');
+// saudacao('Luiza');
+// saudacao('Jorge');
