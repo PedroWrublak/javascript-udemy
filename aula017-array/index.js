6b73873672bfe13ca7bf8a427cd856fdc58ec35c
Pedro Wrublak <pedrovili.w@gmail.com>
2024-09-30 12:18:18 -0300

    arrays (básico)

---
+++ aula017-array/index.js
@@ -0,0 +1,38 @@
+// Arrays (Básico)
+
+// Índice        0        1        2
+const alunos = ['Pedro', 'Maria', 'José']; // Luiza, Eduardo
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(0, -1));
+
+// console.log(alunos[50]);
+
+// delete alunos[1];
+// console.log(alunos);
+
+// const removido = alunos.pop();
+// console.log(removido);
+// console.log(alunos);
+
+// alunos.unshift('Luiza'); // Aciociona no começo
+// alunos.unshift('João');
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('João'); 
+
+// alunos[alunos.length] = 'Luiza' // Adiciona no fim
+// alunos[alunos.length] = 'João'
+// alunos[alunos.length] = 'Paulo'
+
+// alunos[0] = 'Eduardo'; // Altera
+// alunos [3] = 'Luiza'; // Adicionando
+
+
+//console.log(alunos[2]);
+
