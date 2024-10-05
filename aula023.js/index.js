26480883f58ebea5c95e6fcbae67e30ed56cb06e
Pedro Wrublak <pedrovili.w@gmail.com>
2024-10-05 18:36:33 -0300

    operadores lógicos

---
+++ aula023.js/index.js
@@ -0,0 +1,24 @@
+/*
+Operadores Lógicos
+&& - AND -> E -> todas as expressões precisam ser true para retornar true
+|| - OR -> OU
+! - NOT -> NÃO
+*/
+
+// const expressaoAnd = true && true && false && true;
+// console.log(expressaoAnd);
+
+// const expressaoOr = true || false || true;
+// console.log(expressaoOr);
+
+
+console.log(!!false);
+
+/*
+const usuario = 'Pedro'; // form que o usuário digitou
+const senha = '123456'; // form que o usuário digitou
+
+//                       True                 False
+const vaiLogar = usuario === 'Pedro' && senha ==='12345';
+console.log(vaiLogar);
+*/
