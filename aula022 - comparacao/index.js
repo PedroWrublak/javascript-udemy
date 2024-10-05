b7528a73637b3913edca6e54874f96e48c6839be
Pedro Wrublak <pedrovili.w@gmail.com>
2024-10-04 22:52:28 -0300

    operadores de comparação

---
+++ aula022 - comparacao/index.js
@@ -0,0 +1,55 @@
+/*
+Operadores de comparação
+> - maior que
+>= - maior que ou igual a
+< - menor que
+<= - menor que ou igual a
+== - igualdade (valor) - NÃO UTILIZAR
+=== - igualdade estrita (valor e tipo)
+!= - diferença (valor) - NÃO UTILIZAR
+!== - diferença estrita (valor e tipo)
+
+= - atribuição
+*/
+
+// const comp = 10 >= 5; 
+// const comp = 10 >=10;
+// const comp = 10 < 11;
+// const comp = 10 <= 11;
+// console.log(comp);
+
+/*
+const num1 = 10;
+const num2 = 11;
+const comp = num1 < num2
+console.log(comp);
+*/
+
+/*
+const num1 = 10; //number
+const num2 = '10'; //string
+const comp = num1 == num2;
+console.log(comp);
+*/
+
+/*
+const num1 = 10; //number
+const num2 = '10'; //string
+const comp = num1 === num2;
+console.log(comp);
+*/
+
+/*
+const num1 = 10; //number
+const num2 = '10'; //string
+const comp = num1 != num2;
+console.log(comp);
+*/
+
+/*
+const num1 = 10; //number
+const num2 = '10'; //string
+const comp = num1 !== num2;
+console.log(comp);
+*/
+
