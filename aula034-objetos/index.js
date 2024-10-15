5285ef5e50577797c7f7615abf1cf9336e284a24
Pedro Wrublak <pedrovili.w@gmail.com>
2024-10-15 13:36:06 -0300

    Atribuição via desestruturação (Objetos)

--- aula034-objetos/index.js
+++ aula034-objetos/index.js
@@ -1,1 +1,15 @@
 // Atribuição via desestruturação (Objetos)
+
+const pessoa = {
+    nome: 'Pedro',
+    sobrenome: 'Wrublak',
+    idade: 30,
+    endereco: {
+        // rua: 'João Agostinho',
+        numero: 738
+    }
+};
+
+// Atribuição via desestruturação
+const { nome, sobrenome, ...resto } = pessoa;
+console.log(resto);
