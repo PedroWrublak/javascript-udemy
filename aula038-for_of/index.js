6acbb10e243a826e2fce0c2b7731b92e05c85ebc
Pedro Wrublak <pedrovili.w@gmail.com>
2024-10-21 16:27:19 -0300

    for of

--- aula038-for_of/index.js
+++ aula038-for_of/index.js
@@ -1,1 +1,41 @@
 // For of - Estrutura de repetição
+
+//            01234 56...
+const nome = ['Pedro', 'Wrublak', 'Vinicius'];
+
+// For clássico - Geralmente com iteráveis (array ou strings)
+// For in - Retorna o índice ou chave (string, array ou objetos)
+// For of - Retorna o valor em si (iteráveis)
+
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i]);
+}
+
+console.log('###');
+
+for (let i in nome) {
+    console.log(nome[i]);
+}
+
+console.log('###');
+
+for (let valor of nome) {
+    console.log(valor);
+}
+
+console.log('###');
+
+nome.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array);
+});
+
+const pessoa = {
+    nome: 'Pedro',
+    sobrenome: 'Wrublak'
+};
+
+for (let chave in pessoa) {
+    console.log(chave, pessoa[chave])
+};
+
+// for of não funciona com objetos por não serem iteráveis
