f2a937b7130717f86c673ad13d4df86f697ae92f
Pedro Wrublak <pedrovili.w@gmail.com>
2024-11-16 20:46:51 -0300

    arguments

---
+++ aula051-parametros/index.js
@@ -0,0 +1,12 @@
+// Parâmetros da função
+
+// Arguments -> sustenta todos os argumentos enviados
+function funcao() {
+    let total = 0;
+    for (let argumento of arguments) {
+        total += argumento;
+    }
+
+    console.log(total);
+}
+funcao(1, 2, 3, 4, 5, 6, 7);
